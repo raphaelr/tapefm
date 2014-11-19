e902fc287314cd593866633689a1c279197433d0
Raphael Robatsch <raphael-git@tapesoftware.net>
2014-11-19 18:57:49 +0100

    nowin support

--- TapeFM.Server/Code/Program.cs
+++ TapeFM.Server/Code/Program.cs
@@ -1,44 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using Microsoft.Owin.Builder;
-using Microsoft.Owin.Host.HttpListener;
+using Microsoft.Owin.Hosting;
 
 namespace TapeFM.Server.Code
 {
     public static class Program
     {
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            var builder = new AppBuilder();
-            new Startup().Configuration(builder);
+            TapeFmConfig.Load(args);
+            var url = TapeFmConfig.Configuration.Get("host:url") ?? "http://localhost:5000/";
 
-            var uri = TapeFmConfig.Configuration.Get("host:url")
-                      ?? "http://localhost:5000/";
-
-            var properties = new Dictionary<string, object>();
-            properties["host.Addresses"] = new List<IDictionary<string, object>>
+            var options = new StartOptions
             {
-                UriToAddressObject(uri)
+                ServerFactory = TapeFmConfig.Configuration.Get("host:server") ?? "Microsoft.Owin.Host.HttpListener",
             };
+            options.Urls.Add(url);
 
-            OwinServerFactory.Create(builder.Build(), properties);
-            Console.WriteLine("Now listening on {0}", uri);
-            Console.WriteLine("Press enter to stop");
-            Console.ReadLine();
-
-            return 0;
-        }
-
-        private static IDictionary<string, object> UriToAddressObject(string uriString)
-        {
-            var uri = new Uri(uriString);
-            return new Dictionary<string, object>
+            using (WebApp.Start<Startup>(options))
             {
-                {"scheme", uri.Scheme},
-                {"host", uri.Host},
-                {"port", uri.Port.ToString()},
-                {"path", uri.PathAndQuery}
-            };
+                Console.WriteLine("Running server on {0}", url);
+                Console.WriteLine("Press Enter to exit");
+                Console.ReadLine();
+            }
         }
     }
 }
--- TapeFM.Server/Code/TapeFmConfig.cs
+++ TapeFM.Server/Code/TapeFmConfig.cs
@@ -10,12 +10,28 @@ namespace TapeFM.Server.Code
         public static string RedisServer { get; private set; }
         public static int RedisDatabase { get; private set; }
 
-        public static void Load()
+        public static void Load(string[] commandLineArgs = null)
         {
-            Configuration = new Configuration().AddIniFile("tapefm.ini").AddEnvironmentVariables();
+            if (Configuration != null) return;
+            Configuration = CreateConfiguration(commandLineArgs);
+
             LibraryDirectory = Configuration.Get("tapefm:LibraryDirectory");
             RedisServer = Configuration.Get("tapefm:RedisServer");
             RedisDatabase = Configuration.Get<int>("tapefm:RedisDatabase");
+        }
+
+        private static IConfiguration CreateConfiguration(string[] commandLineArgs)
+        {
+            var config = new Configuration();
+
+            config.AddIniFile("tapefm.ini");
+            config.AddEnvironmentVariables();
+            if (commandLineArgs != null)
+            {
+                config.AddCommandLine(commandLineArgs);
+            }
+
+            return config;
         }
     }
 }
